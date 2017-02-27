325b934386527d93ed2f328f0a4546d44f7b768b
hsagarthegr8 <hsagarthegr8@gmail.com>
2017-02-27 20:13:05 +0530

    Added get_faces_with_username function in helper.py

--- helper.py
+++ helper.py
@@ -11,3 +11,19 @@ def ensure_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
         
+import numpy as np
+from PIL import Image
+
+def get_faces_with_username(root_path):
+    usernames = os.listdir(root_path)
+    faces = []
+    users = []
+    for username in usernames:
+        path_to_faces = os.path.join(root_path,username)
+        faces_path = [os.path.join(path_to_faces,img) for img in os.listdir(path_to_faces)]
+        for face in faces_path:
+            face_img = Image.open(face)
+            face_np = np.array(face_img, 'uint8')
+            faces.append(face_np)
+            users.append(username)
+    return np.array(users), faces
