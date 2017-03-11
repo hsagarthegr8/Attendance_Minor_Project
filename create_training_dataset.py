71283732908a316d9b66af65cbd3d56eeff7db5a
hsagarthegr8 <hsagarthegr8@gmail.com>
2017-03-11 12:48:30 +0530

    Added database support

--- create_training_dataset.py
+++ create_training_dataset.py
@@ -5,11 +5,17 @@ Created on Sun Feb 26 22:10:21 2017
 
 @author: hsagarthegr8
 """
+import databasehelper as db
 import cv2, helper
 faceDetect = cv2.CascadeClassifier('Cascade/haarcascade_frontalface_default.xml')
 cam = cv2.VideoCapture(0)
 
-username = input('Enter Username ')
+teacher_id = input('Enter teacher_id: ')
+first_name = input('Enter first_name: ')
+last_name = input('Enter last_name: ')
+gender = input('Enter gender (M/F): ')
+joining_date = input('Enter joining_date (yyyy-mm-dd): ')
+
 sample_count = 0
 
 while True:
@@ -17,12 +23,12 @@ while True:
     gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
     faces = faceDetect.detectMultiScale(gray,1.3,5)
     
-    directory = 'Training/' + username + '/'
+    directory = 'Training/' + teacher_id + '/'
     helper.ensure_dir(directory)
     
     for x,y,w,h in faces:
         sample_count += 1
-        cv2.imwrite(directory + username + '_' + str(sample_count) + '.jpg', gray[y:y+h,x:x+w])
+        cv2.imwrite(directory + teacher_id + '_' + str(sample_count) + '.jpg', gray[y:y+h,x:x+w])
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
         cv2.waitKey(100)
         
@@ -30,7 +36,10 @@ while True:
     cv2.waitKey(1)
     if sample_count >= 20:
         break
-    
+cnx = db.connect()
+db.set_database(cnx)
+db.add_teacher(cnx,teacher_id,first_name,last_name,gender,joining_date)
+cnx.close()
 cam.release()
 cv2.destroyAllWindows()
 
