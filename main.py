6f90649b7fd2f19e6dd2dec6ae4c9b340ed8a99f
Himanshu Sagar <Himanshu Sagar>
2017-04-30 16:40:10 +0530

    Added main file

---
+++ main.py
@@ -0,0 +1,13 @@
+from gui import Ui_Login
+from PyQt5 import QtWidgets
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Login()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
